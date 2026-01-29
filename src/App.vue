37462ec70ca609691b0b17afc29a15a90b9a394c
milla182005 <ludmillalare53@gmail.com>
2026-01-29 16:03:30 +0100

    Sauvegarde avant pull nohaeila

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,5 @@
 </template>
 
 <script setup>
-import NavBar from './components/NavBar.vue'
+import NavBar from '@/components/NavBar.vue'
 </script>
-
-<style>
-/* Ici tu peux mettre le style global si besoin */
-body {
-  margin: 0;
-  font-family: 'Segoe UI', sans-serif;
-}
-</style>
