df2b687a4c19d329984d73c9ab422e50abf62b65
nohaeila <nohaeila.laghalid@ynov.com>
2026-01-29 14:10:59 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import ShoppingCart from './components/ShoppingCart.vue'
 // Logo dans assets
 import Logo from './assets/logo.png'
 
+
 const products = ref([
   { 
     name: 'Nettoyant Visage Hydratant',
